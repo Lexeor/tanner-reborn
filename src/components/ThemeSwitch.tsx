1c97a5f8c1948aba9ffb68b3792c8faf8dd5b4da
Lexeor <lexeor.ross@gmail.com>
2023-05-25 09:44:46 +0400

    ThemeSwitch component migrated

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,19 +1,16 @@
 import React, { useContext } from "react";
-import Sun from "../images/sun.svg";
-import Moon from "../images/moon-solid.svg";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 function ThemeSwitch() {
-  //const [isHover, setIsHover] = useState(false);
   const context = useContext(ThemeContext);
 
   return (
     <div className="settings-icon">
       <i
         className={
-          context.theme === "light" ? "ri-sun-line" : "fa-solid fa-moon"
+          context?.theme === "light" ? "ri-sun-line" : "fa-solid fa-moon"
         }
-        onClick={context.toggleTheme}
+        onClick={context?.toggleTheme}
       />
     </div>
   );
