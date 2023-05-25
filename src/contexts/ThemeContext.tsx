32f47475189eb7dd9349f00d44697de240ec4e4e
Lexeor <lexeor.ross@gmail.com>
2023-05-25 16:02:24 +0400

    Theme load from localStorage added
    Theme load from localStorage added

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type ThemeContextProviderProps = {
   children: React.ReactNode;
@@ -13,16 +13,22 @@ type ThemeContextType = {
 
 const ThemeContext = React.createContext<ThemeContextType | null>(null);
 
+// Get initial theme state from localStorage
+const getInitialState = () => {
+  const theme = localStorage.getItem("savedTheme");
+  return theme ? JSON.parse(theme) : "light";
+};
+
 function ThemeContextProvider({ children }: ThemeContextProviderProps) {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>(getInitialState);
+
+  // Save new theme setting to localStorage
+  useEffect(() => {
+    localStorage.setItem("savedTheme", JSON.stringify(theme));
+  }, [theme]);
 
   function toggleTheme() {
-    setTheme((prev) => {
-      const newTheme = prev === "light" ? "dark" : "light";
-
-      localStorage.setItem("savedTheme", newTheme);
-      return newTheme;
-    });
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
   }
 
   return (
