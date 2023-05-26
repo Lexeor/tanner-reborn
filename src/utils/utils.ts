de311805f7b4f8bae192aa4695b99ea7be90d9bf
Lexeor <lexeor.ross@gmail.com>
2023-05-26 16:03:29 +0400

    compareDates util added

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,10 @@ export function timeFormat(time: number) {
 
   return `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
 }
+
+export function compareDates(date1: Date, date2: Date) {
+  if (date1 < date2) return -1;
+  if (date1 > date2) return 1;
+
+  return 0;
+}
